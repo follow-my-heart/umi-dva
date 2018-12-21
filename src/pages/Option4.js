ec0970ffcbc361f1a4d6a3e7998baa3bff44f5a0
follow-my-heart <874204896@qq.com>
2018-12-21 17:49:08 +0800

    datePicker

--- src/pages/Option4.js
+++ src/pages/Option4.js
@@ -1,8 +1,45 @@
-export default () => {
+import { DatePicker, List } from 'antd-mobile'
+import './test.less'
+import React from 'react'
 
-  return (
-    <div >
-      4
-      </div>
-  )
+function formatDate(date) {
+  const pad = n => n < 10 ? `0${n}` : n;
+  const dateStr = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+  const timeStr = `${pad(date.getHours())}:00`;
+  return `${dateStr} ${timeStr}`;
 }
+
+class Demo extends React.Component {
+  state = {
+    date: null,
+  }
+  render() {
+    return (
+      <List className='list'>
+        <DatePicker
+          className='integer'
+          value={this.state.date}
+          extra='请选择日期'
+          title='开始时间'
+          minDate={new Date()}
+          format={val => formatDate(val)}
+          onChange={date => this.setState({ date })}
+        >
+          <List.Item arrow="horizontal" arrow='empty' activeStyle={{ color: 'red' }}></List.Item>
+        </DatePicker>
+
+        <DatePicker
+          value={this.state.date}
+          extra='请选择日期'
+          title='结束时间'
+          minDate={new Date()}
+          format={val => formatDate(val)}
+          onChange={date => this.setState({ date })}
+        >
+          <List.Item arrow="horizontal" arrow='empty'></List.Item>
+        </DatePicker>
+      </List>
+    );
+  }
+}
+export default Demo;
