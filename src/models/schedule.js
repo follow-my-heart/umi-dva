bcf3521e05558855481e8eeb18ace03fa0566c37
舒珊 <shushan@shuidihuzhu.com>
2019-01-20 11:02:56 +0800

    request

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,37 +1,21 @@
-import queryString from 'query-string'
-import { getSchedule } from '../services/api'
-import lesson from '../services/lesson'
+import queryString from 'query-string';
 
-import { formatMonthData, formatWeekData, countDateTag } from '../utils/dateTool'
+import { formatMonthData, formatWeekData, countDateTag } from '../utils/dateTool';
 let moment = require('moment');
-
-const formatLessonData = (date, lesson) => {
-  let data = date;
-  if (lesson && lesson.length > 0) {
-    lesson.map(v => {
-      if (data.has(v.day)) {
-        data.set(v.day, {
-          ...date.get(v.day),
-          course: v.course,
-          text: `${v.course.length}节课`,
-        })
-      }
-    })
-  }
-  return data
-};
 const today = moment(new Date()).format('YYYY-MM-DD');
-export default {
 
+export default {
   namespace: 'schedule',
 
   state: {
-    tid: null, today, selectKey: today
+    tid: null,
+    today,
+    selectKey: today,
   },
 
   subscriptions: {
     setup({ dispatch, history }) {
-      return history.listen(({ pathname, search }) => {
+      return history.listen(({ search }) => {
         const query = queryString.parse(search);
         dispatch({
           type: 'changeData',
@@ -42,26 +26,17 @@ export default {
   },
 
   effects: {
-    *changeData({ payload }, { call, put }) {
+    *changeData({ payload }, { put }) {
       const { tid, type, n, step, detail } = payload;
-      const calendarData = type === 'weeks'
-        ? formatWeekData(countDateTag(type, new Date(), n))
-        : formatMonthData(countDateTag(type, new Date(), n))
-      const { startTime, endTime } = calendarData;
+      const calendarData =
+        type === 'weeks'
+          ? formatWeekData(countDateTag(type, new Date(), n))
+          : formatMonthData(countDateTag(type, new Date(), n));
       let data = calendarData.data;
       if (data.has(today)) {
-        data.set(today, { ...data.get(today), isSelect: true, day: '今天' })
+        data.set(today, { ...data.get(today), isSelect: true, day: '今天' });
       }
       let date = { ...calendarData, data };
-      const res = yield call(getSchedule, JSON.stringify({
-        start_time: startTime, end_time: endTime
-        // , tid: "170000001"
-      }));
-      if (res.data && res.data.data) {
-        res.data.data.length === 0
-          ? date = { ...calendarData, data, message: '没有课程安排哦' }
-          : data = formatLessonData(calendarData.data, res.data.data.courses)
-      }
       yield put({
         type: 'updateData',
         payload: { tid, type, n, step, detail, date },
